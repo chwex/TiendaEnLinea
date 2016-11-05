1dbe65ccde023782fcd8fddc1159d5e47c0edca8
Carlos Valdez <Carlos Valdez>
2016-11-05 02:48:21 -0700

    agregar carousel a la pagina principal e imagenes

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,9 +4,9 @@
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="{{ asset("/adminlte/bootstrap/css/bootstrap.min.css") }}" >
+  <link rel="stylesheet" href="{{ asset("/css/inicio.css") }}" >
+  
   <style>
     /* Remove the navbar's default rounded borders and increase the bottom margin */
     .navbar {
@@ -35,7 +35,7 @@
   </div>
 </div>
 
-<nav class="navbar navbar-inverse">
+<nav id="mainNav" class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -52,14 +52,92 @@
         <li><a href="#">Deals</a></li>
         <li><a href="#">Stores</a></li>
         <li><a href="#">Contact</a></li>
+        <li class=" dropdown">
+            <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Departments <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li class=" dropdown">
+                    <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View Departments</a>
+                </li>
+                <li><a href="#">Add New</a></li>
+            </ul>
+        </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
       </ul>
+      
     </div>
   </div>
 </nav>
+
+<div id="mainCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#mainCarousel" data-slide-to="1"></li>
+    <li data-target="#mainCarousel" data-slide-to="2"></li>
+    <li data-target="#mainCarousel" data-slide-to="3"></li>
+    <li data-target="#mainCarousel" data-slide-to="4"></li>
+    <li data-target="#mainCarousel" data-slide-to="5"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="img-responsive center-block" src="{{ asset("/Imagenes/car1.jpg") }}" alt="Image">
+      <div class="carousel-caption">
+        <h3>Carreras</h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="item">
+      <img class="img-responsive center-block" src="{{ asset("/Imagenes/car2.jpg") }}" alt="Image">
+      <div class="carousel-caption">
+        <h3>FarCry</h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="item">
+      <img class="img-responsive center-block" src="{{ asset("/Imagenes/car3.jpg") }}" alt="Image">
+      <div class="carousel-caption">
+        <h3>Nintendo</h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="item">
+      <img class="img-responsive center-block" src="{{ asset("/Imagenes/car4.jpg") }}" alt="Image">
+      <div class="carousel-caption">
+        <h3>Surgeon Simulator</h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="item">
+      <img class="img-responsive center-block" src="{{ asset("/Imagenes/car5.jpg") }}" alt="Image">
+      <div class="carousel-caption">
+        <h3>Social Media</h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="item">
+      <img class="img-responsive center-block" src="{{ asset("/Imagenes/car6.jpg") }}" alt="Image">
+      <div class="carousel-caption">
+        <h3>League of Legends</h3>
+        <p></p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#mainCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#mainCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 <div class="container">
   <div class="row">
@@ -120,6 +198,9 @@
     <button type="button" class="btn btn-danger">Sign Up</button>
   </form>
 </footer>
+
+<script src="{{ asset("/adminlte/plugins/jQuery/jquery-2.2.3.min.js") }}"></script>
+<script src="{{ asset("/adminlte/bootstrap/js/bootstrap.min.js") }}"></script>
 
 </body>
 </html>
