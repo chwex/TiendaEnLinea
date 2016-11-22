dc7f1aec3fcda78323e0e3bed6f047078866c151
Marcos Orozco <dexter_mister@hotmail.com>
2016-11-22 10:55:11 -0700

    Se deja habilitado comentarios

--- app/Http/Controllers/comentariosController.php
+++ app/Http/Controllers/comentariosController.php
@@ -4,7 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\productos;
+use App\categorias;	
+use App\comentarios;			#Añadi la tabla
+use DB;
+use App\Http\Requests;
+use App\http\Controllers\Controller; 
+use Session;
+use Redirect;
+
 class comentariosController extends Controller
 {
-    //
+    public function guardar(Request $request){
+    	$idproducto=$request->input('idproducto');
+    	$mensaje=$request->input('mensaje');
+        $idusuario = \Auth::user()->id;
+
+    	//Guardar en BD
+
+    	$nuevo = new comentarios;
+    	$nuevo->idusuario= $idusuario;
+        $nuevo->idproducto= $idproducto;
+    	$nuevo->mensaje=$mensaje;
+    	$nuevo->save();
+
+    	return Redirect('/productos/'.$idproducto);
+
+    } 
 }
