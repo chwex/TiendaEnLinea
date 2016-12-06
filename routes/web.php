d610ad0f9170980a9b69aee8e0710ea292531be7
Carlos Valdez <Carlos Valdez>
2016-12-06 08:06:55 -0700

    ruta para mostrar ventas

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::group(['middleware' => ['admin']], function(){
     Route::get('image-upload','ImageController@imageUpload');
     Route::post('image-upload','ImageController@imageUploadPost');
     Route::get('/mostrarCategoria', 'categoriasController@mostrarCategoria');
-
+    Route::get('/mostrarVentas','ventasController@obtenerVentas');
     //Route::get('/');
     Route::post('/guardarVoto','comentariosController@guardar');
     //Rutas carrito
@@ -63,4 +63,4 @@ Route::post('/removerProductoCarrito', 'carritousuarioController@removerProducto
 Route::post('/guardarComentario','comentariosController@guardar');
 Route::get('/micarrito','carritousuarioController@obtenerCarrito');
 Route::get('/agregarCarrito/{idproducto}', 'carritousuarioController@agregarCarrito');
-Route::get('/mostrarVentas','ventasController@obtenerVentas');
+
