6ce641c1aa727b2ad2210972e11b8e4958e0a0f2
OrlandoDavid <david_kook@hotmail.com>
2016-12-06 16:38:01 -0700

    Correcciones

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,13 +5,13 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Registro usuario</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">Nombre</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
@@ -53,10 +53,10 @@
                         </div>  
 
                                <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label"> telefono </label>
+                            <label for="email" class="col-md-4 control-label"> Telefono </label>
                                
                                <div class="col-md-6">
-                                    <input id="telefono" type="telefono" class="form-control" name="telefono" value="{{ old('telefono') }}" required>
+                                    <input id="telefono" type=number class="form-control" name="telefono" value="{{ old('telefono') }}" required>
 
                                     @if ($errors->has('telefono'))
                                         <span class="help-block">
@@ -70,19 +70,23 @@
                             <label for="email" class="col-md-4 control-label"> Sexo </label>
                                
                                <div class="col-md-6">
-                                    <input id="sexo" type="sexo" class="form-control" name="sexo" value="{{ old('sexo') }}" required>
-
+                                    
+                                    <select id="sexo" type="sexo" class="form-control" name="sexo" value="{{ old('sexo') }}" required>
+                                        <option value="">Sexo:</option>
+					                    <option value="0">Femenino</option>
+					                    <option value="1">Masculino</option>
                                     @if ($errors->has('sexo'))
                                         <span class="help-block">
                                             <strong>{{ $errors->first('sexo') }}</strong>
                                         </span>
                                     @endif
+                                    </select>
                               </div>
                         </div>  
 
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Correo electronico</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -96,7 +100,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Contraseña</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -110,7 +114,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 control-label">Confirmacion de contraseña</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -120,7 +124,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Registro
                                 </button>
                             </div>
                         </div>
